b094ea1a2b7e16e85ef0ef1077e5989a354c252d
rupesh-dc <rupesh.mishra@datachannel.co>
2024-03-19 20:30:18 +0530

    removed postgres adapter settings as it was not working

--- src/auth.ts
+++ src/auth.ts
@@ -1,17 +1,6 @@
 import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import PostgresAdapter from "@auth/pg-adapter";
-import { Pool } from "pg";
-
-const pool = new Pool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-});
 
 const credentialsConfig = CredentialsProvider({
     name: "Credentials",
@@ -41,7 +30,6 @@ const credentialsConfig = CredentialsProvider({
 });
 
 const config = {
-    adapter: PostgresAdapter(pool),
     providers: [credentialsConfig],
 } satisfies NextAuthConfig;
 
