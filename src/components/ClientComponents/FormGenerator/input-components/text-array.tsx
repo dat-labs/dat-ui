f652335c3dbd72af4f6dae879c7803b8727064cc
rupesh-dc <rupesh.mishra@datachannel.co>
2024-06-10 16:26:10 +0530

    added default values support for textarray

--- src/components/ClientComponents/FormGenerator/input-components/text-array.tsx
+++ src/components/ClientComponents/FormGenerator/input-components/text-array.tsx
@@ -1,20 +1,7 @@
 import React from "react";
 import { useFormContext } from "react-hook-form";
 import { Input } from "@/components/ui/input";
-
-/**
- * Custom Chip component
- */
-const Chip = ({ value, onDelete }) => {
-    return (
-        <div className="flex items-center rounded-md bg-muted px-2 py-1 text-sm">
-            {value}
-            <button type="button" className="ml-2 rounded-md p-1 text-red-500 hover:bg-red-100" onClick={onDelete}>
-                &times;
-            </button>
-        </div>
-    );
-};
+import Chip from "@/components/commom/chip";
 
 export default function TextArray({ field_name, description, defaultValue }) {
     const { getValues, setValue, watch } = useFormContext();
