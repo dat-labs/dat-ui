717290f6ef5d37306b510f02fd1b2c5b608fc8b3
abhinav-DC <abhinav@datachannel.co>
2024-09-03 13:38:47 +0530

    Formgenerator added for new workspace form

--- src/components/ClientComponents/addWorkspace/addWorkspaceForm.tsx
+++ src/components/ClientComponents/addWorkspace/addWorkspaceForm.tsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import { Label } from "../../ui/label";
-import { Input } from "../../ui/input";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import {
     Dialog,
     DialogClose,
@@ -12,29 +9,22 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from "../../ui/button";
-import { useForm } from "react-hook-form";
 import { PlusCircledIcon } from "@radix-ui/react-icons";
 import { getSession } from "next-auth/react";
 import useApiCall from "@/hooks/useApiCall";
 import { createWorkspace } from "./api";
-import CircularLoader from "@/components/ui/circularLoader";
 import { toast } from "sonner";
+import FormGenerator from "../FormGenerator";
 
 export default function AddWorkspaceForm({ postSubmissionAction }: { postSubmissionAction: any }) {
     const [open, setOpen] = useState(false);
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        reset,
-        formState: { errors, isSubmitting },
-    } = useForm();
+    const [error, setError] = useState("");
+    error && setError(null);
 
     const { makeApiCall } = useApiCall(createWorkspace);
     const onSubmit = async (data) => {
         const session = await getSession();
-        const body = { ...data, organization_id: session.user.organization_id };
+        const body = { ...data, organization_id: session.user.organization_id, status: "active" };
         const res = await makeApiCall(body);
 
         if (res.status === 200) {
@@ -48,80 +38,31 @@ export default function AddWorkspaceForm({ postSubmissionAction }: { postSubmiss
 
     const handleDialogOpen = (isOpen) => {
         setOpen(isOpen);
-        reset();
     };
+
+    const props: any = {
+        name: {
+            title: "New Workspace name",
+            type: "string",
+            field_name: "name",
+        },
+    };
+
+    const required_fields = ["name"];
 
     return (
         <Dialog open={open} onOpenChange={handleDialogOpen}>
             <DialogTrigger asChild>
-                <span className="m-auto cursor-pointer hover:bg-primary/10 p-2 pr-3">
-                    <PlusCircledIcon className="size-6" />
-                </span>
+                <PlusCircledIcon className="size-6" />
             </DialogTrigger>
             <DialogContent className="max-w-[450px] h-fit z-50">
-                <DialogHeader>
-                    <DialogTitle>Add a new workspace</DialogTitle>
-                </DialogHeader>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="flex flex-col gap-4 py-4 m-0 p-0">
-                        <div className="flex flex-col gap-1">
-                            <div className="flex items-center gap-4">
-                                <Label htmlFor="name" className="text-left">
-                                    Name
-                                </Label>
-                                <Input
-                                    id="name"
-                                    placeholder="Enter new Workspace name"
-                                    autoFocus
-                                    {...register("name", {
-                                        required: "Name is required",
-                                        minLength: {
-                                            value: 4,
-                                            message: "Name must be at least 4 characters",
-                                        },
-                                    })}
-                                />
-                            </div>
-
-                            {errors.name && <p className="text-sm text-right text-red-500">{errors.name.message}</p>}
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <div className="flex items-center gap-4">
-                                <Label htmlFor="status" className="text-left">
-                                    Status
-                                </Label>
-                                <Select
-                                    {...register("status", {
-                                        required: "Status is required",
-                                    })}
-                                    onValueChange={(value) => setValue("status", value)}
-                                >
-                                    <SelectTrigger>
-                                        <SelectValue placeholder="Select status" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectGroup>
-                                            <SelectItem value="active">active</SelectItem>
-                                            <SelectItem value="inactive">inactive</SelectItem>
-                                        </SelectGroup>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-
-                            {errors.status && <p className="text-sm text-right text-red-500">{errors.status.message}</p>}
-                        </div>
-                    </div>
-                    <DialogFooter className="mt-4">
-                        <DialogClose>
-                            <Button type="button" variant="outline" className="mr-2" onClick={() => setOpen(false)}>
-                                Cancel
-                            </Button>
-                        </DialogClose>
-                        <Button type="submit" disabled={isSubmitting}>
-                            {isSubmitting ? <CircularLoader /> : "Save"}
-                        </Button>
-                    </DialogFooter>
-                </form>
+                <FormGenerator
+                    properties={props}
+                    onSubmit={onSubmit}
+                    errorText={error}
+                    required_fields={required_fields}
+                    submitButtonText="Submit"
+                />
             </DialogContent>
         </Dialog>
     );
